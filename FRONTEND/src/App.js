301c88572acebc3adf5274ea72872e44305d3e38
James <jameslarbie30@gmail.com>
2024-07-06 19:12:15 +0000

    added modal

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+
 import "./App.css";
 import ScoreBoard from "./components/ScoreBoard";
 import GameBoard from "./GameBoard";
+import modalImage from "./images/image-rules.svg"
+import closeIcon from "./images/icon-close.svg"
+
 
 function App() {
+	const handlemodalShow = () => {
+		const modal = document.querySelector("dialog")
+		modal.show()
+	}
+
+	const handlecloseModal = () => {
+		const modal = document.querySelector("dialog")
+		modal.close()
+	}
+	
 	return (
 		<div>
 			<ScoreBoard />
 
 			<GameBoard />
 
-			<button className="rules">RULES</button>
+			<dialog>
+				<div>
+					<h1>RULES</h1>
+					<button onClick={handlecloseModal}><img src={closeIcon} alt=""/></button>
+				</div>
+				<img src={modalImage} alt=""/>
+			</dialog>
+			
+			<button className="rules" onClick={handlemodalShow}>RULES</button>
 		</div>
 	);
 }
