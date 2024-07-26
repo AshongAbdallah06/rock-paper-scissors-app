eda0c257f279e39bd4daf58dbcc20ea5cbb2d721
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-07-26 20:03:12 +0000

    Create live chat feature

---
+++ FRONTEND/src/components/Chat.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef, useState } from "react";
+import closeIcon from "../images/icon-close-chat.svg";
+import useCheckContext from "../hooks/useCheckContext";
+
+const Chat = () => {
+	const { playerMove, setChatIsShowing, socket } = useCheckContext();
+	const [textMessage, setTextMessage] = useState("");
+	const [messages, setMessages] = useState(JSON.parse(localStorage.getItem("messages")) || []);
+
+	useEffect(() => {
+		socket.on("message", (message) => {
+			setMessages((prevMessages) => [...prevMessages, message]);
+		});
+
+		return () => {
+			socket.off("message");
+		};
+	}, []);
+
+	const messagesEndRef = useRef(null);
+	useEffect(() => {
+		localStorage.setItem("messages", JSON.stringify(messages));
+
+		// Scroll to bottom
+		messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+	}, [messages]);
+
+	const sendMessage = () => {
+		socket.emit("message", textMessage);
+
+		setTextMessage("");
+	};
+
+	return (
+		<aside>
+			<div className="content">
+				<header>
+					<h1>Live Chat</h1>
+					<img
+						src={closeIcon}
+						alt="close-icon"
+						className="close-icon"
+						onClick={() => setChatIsShowing(false)}
+					/>
+				</header>
+
+				<div className="chat-container">
+					<div className="messages">
+						{messages.map((message, index) => (
+							<div
+								className="person"
+								key={index}
+							>
+								<p className="name">Ashong</p>
+								<p className="message">{message}</p>
+							</div>
+						))}
+						<div ref={messagesEndRef} />
+					</div>
+
+					<div className="input-container">
+						<textarea
+							type="text"
+							value={textMessage}
+							placeholder="Enter text message"
+							onChange={(e) => {
+								setTextMessage(e.target.value);
+							}}
+						></textarea>
+						<button
+							onClick={() => {
+								textMessage !== "" && sendMessage();
+							}}
+						>
+							Send
+						</button>
+					</div>
+				</div>
+			</div>
+		</aside>
+	);
+};
+
+export default Chat;
--- FRONTEND/src/context/CheckContextProvider.js
+++ FRONTEND/src/context/CheckContextProvider.js
@@ -2,7 +2,9 @@ import React, { createContext, useEffect, useState } from "react";
 import scissorsIcon from "../images/icon-scissors.svg";
 import paperIcon from "../images/icon-paper.svg";
 import rockIcon from "../images/icon-rock.svg";
+import io from "socket.io-client";
 
+const socket = io("http://localhost:4001");
 export const CheckContext = createContext();
 const CheckContextProvider = ({ children }) => {
 	const [playerMove, setPlayerMove] = useState(null);
@@ -21,6 +23,8 @@ const CheckContextProvider = ({ children }) => {
 	useEffect(() => {
 		localStorage.setItem("score", JSON.stringify(score));
 	}, [score]);
+
+	const [chatIsShowing, setChatIsShowing] = useState(false);
 
 	const checkOptions = () => {
 		setResult("...");
@@ -86,6 +90,12 @@ const CheckContextProvider = ({ children }) => {
 		checkOptions();
 	}, [playerMove, computerMove]);
 
+	const makeMove = (move) => {
+		socket.emit("move", move);
+
+		// setTextMessage("");
+	};
+
 	return (
 		<CheckContext.Provider
 			value={{
@@ -101,6 +111,10 @@ const CheckContextProvider = ({ children }) => {
 				computerMoveImage,
 				userExist,
 				setUserExist,
+				chatIsShowing,
+				setChatIsShowing,
+				socket,
+				makeMove,
 			}}
 		>
 			{children}
