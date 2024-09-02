5199f8cf55e428cb024016022df8e58459e33fdb
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-09-02 03:16:24 +0000

    Fix bug

--- FRONTEND/src/pages/Profile.js
+++ FRONTEND/src/pages/Profile.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import profileIcon from "../images/person-circle-outline.svg";
 import logo from "../images/logo.svg";
 import { Link } from "react-router-dom";
 import useCheckContext from "../hooks/useCheckContext";
+import Axios from "axios";
 
 const Profile = () => {
 	const user = JSON.parse(localStorage.getItem("user"));
-	const { stats } = useCheckContext();
+	const { stats, setStats, isOnePlayer } = useCheckContext();
+
+	const getUserStats = async () => {
+		try {
+			const res = await Axios.get(`http://localhost:4001/api/user/stats/${user.username}`);
+			const data = res.data[0];
+
+			setStats((prevStats) => ({
+				...prevStats,
+				score: data.score,
+				gamesPlayed: data.games_played,
+				lastPlayed: data.last_played,
+				loses: data.loses,
+				ties: data.ties,
+				wins: data.wins,
+				username: user.username,
+			}));
+
+			// setScore(stats.score)
+		} catch (error) {
+			console.error("🚀 ~ getUserStats ~ error:", error);
+		}
+	};
+
+	useEffect(() => {
+		if (isOnePlayer) {
+			getUserStats();
+
+			console.log("Getting", stats);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (isOnePlayer) {
+			getUserStats();
+
+			console.log("HI", stats);
+		}
+	}, [stats]);
 
 	return (
 		<>
